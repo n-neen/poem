54dc1a968974ec1fe51f75aff18917523aae97ac
n-neen <115341144+n-neen@users.noreply.github.com>
2026-03-24 07:47:42 -0400

    name

--- newgame.bat
+++
@@ -1,3 +0,0 @@
-"E:\Program Files (x86)\Emulators\super metroid\asar\asar.exe" --fix-checksum=on --symbols=wla --symbols-path="C:\Users\keenan\Desktop\newgame_repo\robotpast\robotpast.sym" --no-title-check -v C:\Users\keenan\Desktop\newgame_repo\robotpast\robotpast.asm C:\Users\keenan\Desktop\newgame_repo\robotpast\robotpast.sfc
-
-pause
---
+++ poem.bat
@@ -0,0 +1,3 @@
+"E:\Program Files (x86)\Emulators\super metroid\asar\asar.exe" --fix-checksum=on --symbols=wla --symbols-path="C:\Users\keenan\Desktop\newgame_repo\robotpast\poem.sym" --no-title-check -v C:\Users\keenan\Desktop\newgame_repo\robotpast\poem.asm C:\Users\keenan\Desktop\newgame_repo\robotpast\poem.sfc
+
+pause
